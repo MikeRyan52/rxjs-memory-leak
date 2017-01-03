e31a5f0c7e0ef75ef2aeaccd8e42999c8850712d
Mike Ryan <mike.ryan52@gmail.com>
2017-01-03 13:18:23 -0600

    Demonstrating memory leak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import 'rxjs/add/operator/observeOn';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs/observable/interval';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { queue } from 'rxjs/scheduler/queue';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  count$ = new BehaviorSubject(0);
+
+  ngOnInit() {
+    interval(50).observeOn(queue).subscribe(this.count$);
+  }
 }
